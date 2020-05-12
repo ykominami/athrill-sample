b1bf058a48f093fdc683a0fb017c199e9c41a9d1
yasuo kominami <ykominami@gmail.com>
2020-05-13 00:17:25 +0900

    add asp3/workspace

---
+++ ev3rt/ev3rt-beta7-release/asp3/workspace/mruby_app/speaker_sample.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+include EV3RT_TECS
+
+begin
+    LCD.font=:medium
+ 
+    LCD.puts "speaker sample"
+    
+    Speaker.volume= 1
+    
+    #[:f4, 100, :f4, 100, :f4, 100, :f4, 100,:e4, 100,:g4, 100,:f4, 100].each_slice(2){|tone,duration| #本当はこう書きたい
+    [[:f5, 150], [:f5, 150], [:f5, 150], [:f5, 200],[:e5, 300],[:g5, 300],[:f5, 400]].each do |tone_duration|
+        LCD.puts tone_duration
+        Speaker.tone(tone_duration[0], tone_duration[1])
+        RTOS.delay(tone_duration[1])
+    end 
+rescue => e
+    LCD.error_puts e
+end
